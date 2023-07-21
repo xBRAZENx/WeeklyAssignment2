7f4478d6a00684539f06905404ece938f53016d8
xBRAZENx <guptaatharva727@gmail.com>
2023-07-21 11:52:46 +0530

    Initial commit

---
+++ LargeDataCollection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyAssignment2
+{
+    public class LargeDataCollection : IDisposable
+    {
+        public List<string> list= new List<string>();
+        bool disposedValue;int listCount;
+        public LargeDataCollection()
+        {
+            Console.WriteLine("Enter number of items to be added to List");
+            listCount = int.Parse(Console.ReadLine());
+            for(int i = 0; i < listCount; i++)
+            {
+                
+                list.Add(Console.ReadLine());
+            }
+        }
+        public void Add(string item) { list.Add(item);listCount++; }
+        public void Remove(string item) { list.Remove(item);}
+        public void AccessData(int index)
+        {
+            Console.WriteLine(list.ElementAt(index));
+        }
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                   listCount = 0;
+                }
+                list = null; disposedValue = true;
+                Console.WriteLine("List has been nullified");
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyAssignment2
+{
+    public class Program
+    {
+        public List<string> listArray = new List<string>();
+        static void Main(string[] args)
+        {
+            string item;string choice;
+            LargeDataCollection obj = new LargeDataCollection();
+            do
+            {
+                Console.WriteLine("Enter operations as follows \n1.Add\n2.Remove\n3.Acess Data");
+                int opt =int.Parse(Console.ReadLine());
+                switch(opt)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Enter item to be added");
+                            item = Console.ReadLine();
+                            obj.Add(item); break;
+                        }
+                    case 2: { Console.WriteLine("Enter name you want to remove");
+                        item = Console.ReadLine();
+                        obj.Remove(item); break;}
+                    case 3:
+                        {
+                            Console.WriteLine("Enter index of item to access");
+                            obj.AccessData(int.Parse(Console.ReadLine()));break;
+                        }
+                    default: { Console.WriteLine("Invalid Input");break; }
+                }
+                Console.WriteLine("Press y to continue");
+                choice = Console.ReadLine().ToLower();
+
+            }
+            while(choice == "y");
+            Console.WriteLine("All Data has been Disposed");
+            obj.Dispose();
+            Console.ReadKey();
+        }
+    }
+}
